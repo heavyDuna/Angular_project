38047ddeb1a6af976576f2f5a090e6363d559ace
Sandra <sandrarodriguez.hd@gmail.com>
2020-10-15 19:32:17 +0200

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
+import { PeliculaComponent } from './pages/pelicula/pelicula.component';
+import { SearchComponent } from './pages/search/search.component';
 
-const routes: Routes = [];
+//definición de rutas
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
 
+  {
+    path: 'pelicula/:id',
+    component: PeliculaComponent
+  },
+
+  {
+    path: 'search/:texto',
+    component: SearchComponent
+  },
+
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
+
+//crea modulos internamente
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes) //forRoot rutas principales, forChild rutas hijas
+  ],
+
+  //exportarlo para que la app pueda utilizarlo importandolo
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +11,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
