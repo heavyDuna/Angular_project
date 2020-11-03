13eb441aeb6346f7514e97ac775dfda6628b7498
Sandra <sandrarodriguez.hd@gmail.com>
2020-11-03 21:08:23 +0100

    Crear componente grid de peliculas

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { SliderComponent } from './slider/slider.component';
+import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    SliderComponent
+    SliderComponent,
+    PeliculasPosterGridComponent
   ],
   exports: [
     NavbarComponent,
-    SliderComponent
+    SliderComponent,
+    PeliculasPosterGridComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/components/peliculas-poster-grid/peliculas-poster-grid.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Pelicula } from 'src/app/interfaces/cartelera-respuesta';
+
+@Component({
+  selector: 'app-peliculas-poster-grid',
+  templateUrl: './peliculas-poster-grid.component.html',
+  styleUrls: ['./peliculas-poster-grid.component.css']
+})
+export class PeliculasPosterGridComponent implements OnInit {
+
+  @Input() peliculas : Pelicula[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.peliculas);
+  }
+
+}
