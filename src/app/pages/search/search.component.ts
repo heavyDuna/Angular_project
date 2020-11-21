1509f79f69de2038636162ccdce6a4aba414906e
Sandra <sandrarodriguez.hd@gmail.com>
2020-11-21 20:37:32 +0100

    Routing para que al buscar lleve a pantalla de search

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PeliculasService } from 'src/app/services/peliculas.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute,      //para obtener el valor de la url
+               private peliculasServicio: PeliculasService ) {        //inyecto el servicio
+
+  }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe( params => {
+      this.peliculasServicio.buscarPeliculas( params.texto ).subscribe( peliculas => {
+        //console.log(peliculas[0].title);
+      })
+    })
+
   }
 
 }
