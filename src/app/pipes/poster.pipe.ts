71a50dba9bdaf966116a3824ced145d06018f78f
Sandra <sandrarodriguez.hd@gmail.com>
2020-11-21 18:33:56 +0100

    Pequeño fix en poster pipe

--- src/app/pipes/poster.pipe.ts
+++ src/app/pipes/poster.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PosterPipe implements PipeTransform {
 
   transform( posterpelicula: string ): string {     //recibimos el "id" del poster_path, que es un string y devolvemos también un string
-    console.log(posterpelicula)
+    //console.log(posterpelicula)
     if ( posterpelicula ) {
       return `https:image.tmdb.org/t/p/w500${ posterpelicula }`;
     }else {
-      return 'https:image.tmdb.org/t/p/w500oVY4gUXsOfSQIWm43fRFNK8GZYs.jpg';
+      return 'https:image.tmdb.org/t/p/w500/oVY4gUXsOfSQIWm43fRFNK8GZYs.jpg';
     }
   }
 
