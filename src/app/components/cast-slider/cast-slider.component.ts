53cad3a422d1056163a73b3de13539e6fca0ba27
Sandra <sandrarodriguez.hd@gmail.com>
2020-11-29 21:30:08 +0100

    Crear componente cast slider, inicializar, html y css, e interface del cast

---
+++ src/app/components/cast-slider/cast-slider.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Cast } from 'src/app/interfaces/creditos-respuesta';
+import Swiper from 'swiper';
+
+@Component({
+  selector: 'app-cast-slider',
+  templateUrl: './cast-slider.component.html',
+  styleUrls: ['./cast-slider.component.css']
+})
+export class CastSliderComponent implements OnInit, AfterViewInit {
+
+  @Input() cast: Cast[];                  //recibiendo la info mediante el input
+
+  public Swiper: Swiper;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.Swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5.3,
+    freeMode: true,
+    spaceBetween: 15,
+    });
+  }
+
+  ngOnInit(): void {
+    //console.log(this.cast)
+  }
+
+}
---
+++ src/app/interfaces/creditos-respuesta.ts
@@ -0,0 +1,21 @@
+export interface CreditosRespuesta {
+  id:   number;
+  cast: Cast[];
+}
+
+export interface Cast {
+  adult:                boolean;
+  gender:               number;
+  id:                   number;
+  known_for_department: string;
+  name:                 string;
+  original_name:        string;
+  popularity:           number;
+  profile_path:         null | string;
+  cast_id?:             number;
+  character?:           string;
+  credit_id:            string;
+  order?:               number;
+  department?:          string;
+  job?:                 string;
+}
